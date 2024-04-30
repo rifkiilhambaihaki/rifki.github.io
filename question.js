f59bc509718367564ca92f0adc8cc6608361d42c
rifkiilhambaihaki <rifkiilham63@gmail.com>
2024-04-30 08:38:57 +0700

    first commit

---
+++ question.js
@@ -0,0 +1,52 @@
+let questions = [
+  {
+    numb: 1,
+    questions: "Apa kepanjangan dari HTML?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multi Language",
+      "C. Hyper Text Markup Language",
+      "D. Home Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    questions: "Apa kepanjangan dari CSS?",
+    answer: "B. Cascading Style Sheet",
+    options: [
+      "A. Cute Style Sheet",
+      "B. Cascading Style Sheet",
+      "C. Codehal Style Sheet",
+      "D. Computer Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    questions: "Apa platform Google untuk belajar machine learning?",
+    answer: "A. Google Teachable Machine",
+    options: [
+      "A. Google Teachable Machine",
+      "B. Google Tensorflow",
+      "C. Google Learning Machine",
+      "D. Google Tensor Machine",
+    ],
+  },
+  {
+    numb: 4,
+    questions: "Apa nama awal dari Javascript?",
+    answer: "B. Mocha",
+    options: ["A. Superscript", "B. Mocha", "C. Livescript", "D. Ajaxscript"],
+  },
+  {
+    numb: 5,
+    questions: "Apa nama website untuk menunjukkan hasil karya kita?",
+    answer: "D. Portofolio Website",
+    options: [
+      "A. CV Website",
+      "B. Experience Website",
+      "C. Creation Website",
+      "D. Portofolio Website",
+    ],
+  },
+];
